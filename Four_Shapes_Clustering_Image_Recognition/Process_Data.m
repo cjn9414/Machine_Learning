2241c5b15c4dd67a9ad3559a44eed44fe132e2a9
Carter Nesbitt <ccnesbitt@gmail.com>
2018-05-19 20:57:30 -0400

    Created function to examine images from dataset and place them in a .mat file. Further optimizations for this functions will come in the future.

---
+++ Four_Shapes_Clustering_Image_Recognition/Process_Data.m
@@ -0,0 +1,59 @@
+imgsize = [200 200];
+
+triangle_dir = './triangle';
+circle_dir = './circle';
+star_dir = './star';
+square_dir = './square';
+
+
+fprintf('Loading all triangles...\n')
+triangles = dir([triangle_dir, '/*.png']);
+
+fprintf('Loading all circles...\n')
+circles = dir([circle_dir, '/*.png']);
+
+fprintf('Loading all stars...\n')
+stars = dir([star_dir, '/*.png']);
+
+fprintf('Loading all squares...\n')
+squares = dir([square_dir, '/*.png']);
+
+shapes = [triangles(1:1000); circles(1:1000); stars(1:1000); squares(1:1000)];
+
+clear triangles circles stars squares;
+
+fprintf("All shape data has been loaded into MATLAB. Press any button to reformat the data\n")
+pause
+
+const = 8; % Used to determine size of data file
+m = (size(shapes, 1))/const;
+data = zeros(m, imgsize(1)*imgsize(2));
+
+for i = 1:const:size(shapes, 1)
+    fname = shapes(i).name;
+    if (i <= 3720)
+      folder = 'triangle/';
+    elseif (i <= 3720*2)
+      folder = 'circle/';
+    elseif (i <= (3720*2) + 3765)
+      folder = 'star/';
+    else 
+      folder = 'square/';
+    end
+    data((const+i-1)/const, :) = (imread([folder fname])(:))';
+end
+
+fprintf("All data has been reformatted. Press any button to write the data into a file.\n")
+pause
+
+n = size(data, 1);
+idx = randperm(n);
+data_shuffle = zeros(size(data));
+for i = 1:n
+  data_shuffle(i, :) = data(idx(i), :);
+end
+
+data = uint8(data_shuffle);
+save('data.mat', 'data');
+
+clear all;
--- Four_Shapes_Clustering_Image_Recognition/README.md
+++ Four_Shapes_Clustering_Image_Recognition/README.md
@@ -1,1 +1,5 @@
 This repository is being used to interpret a set of 16000 images originating from the Four Shapes Dataset. This dataset contains images of stars, squares, circles, and triangles, all of which are rotated freely. I am going to attempt to use a clustering algorithm in order to accurately classify these four shapes in the dataset. I am going to create this program using the programming language Octave.
+
+The link to the dataset is located below:
+
+https://www.kaggle.com/smeschke/four-shapes
